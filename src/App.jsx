5ca02f39c31b92ae9a683820ea8e4fbc78778311
SebasDev <sebastianoliverosxd2002@gmail.com>
2025-04-09 11:25:08 -0500

    update 2.0 fronted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../src/pages/Login.jsx';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
