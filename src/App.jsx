e0c8135e3d7b9df2cf5b0b3d2dc64779709a224e
SebasDev <sebastianoliverosxd2002@gmail.com>
2025-04-08 21:09:02 -0500

    Fronted update Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from '../src/pages/Login.jsx';
 
 function App() {
   return (
-    
-    <h1 className=' text-6xl text-blue-500'>Hi world</h1>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
